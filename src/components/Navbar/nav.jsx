c516354bc3dbdfeb2f399d4b27c94b9b6e1c5895
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-07-01 04:03:00 +0000

    Update nav.jsx

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -5,13 +5,13 @@ const nav = () => {
 <div>
    <nav className='flex'>
           {/* logo */}
-        <h1 className='bg-orange text-3xl font-extrabold text-white ml-10'>
+        <h1 className='bg-orange text-3xl font-extrabold text-white ml-10 nav_logo'>
             TheSkillzHub
         </h1>
 
          {/* nav_links */}
          <div className='text-white flex text-center '>
-              <ul className='flex text-xl gap-4 font-semibold nav'>
+              <ul className='flex text-xl gap-4 font-semibold nav nav_text'>
                   <li><a href="#Home"></a>Home</li>
                   <li><a href="#About"></a>About</li>
                   <li><a href="#Courses"></a>Courses</li>
