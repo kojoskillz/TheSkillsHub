aba2a05fa80ada674c3c669d4f5e2af63bb8505b
Kwadwo Nyarko <115382951+kojoskillz@users.noreply.github.com>
2024-07-05 03:30:09 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
-
+import React from 'react';
+import './App.css';
 import Navbar from './components/Navbar/nav';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Footer from './components/Footer/Footer';
+
+
+
+
 
 const App = () => {
   return (
     <>
         <Navbar />
         <Home />
-        <Login/>
+        <Login />
         <Footer />
 
     </>
